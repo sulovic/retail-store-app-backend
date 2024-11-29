de99a0cc1fbac9bde0cb120d09295af51c2302b4
Vladimir Sulovic <sulovic@gmail.com>
2024-11-29 12:51:35 +0100

    Implemented bulk upload route

--- routes/productRoutes.ts
+++ routes/productRoutes.ts
@@ -7,6 +7,7 @@ router.get("/", productController.getAllProductsController);
 router.get("/count", productController.getAllProductsCountController);
 router.get("/:productId", productController.getProductController);
 router.post("/", productController.createProductController);
+router.post("/bulk-upload", productController.bulkUploadProductsController);
 router.put("/:productId", productController.updateProductController);
 router.delete("/:productId", productController.deleteProductController);
 
