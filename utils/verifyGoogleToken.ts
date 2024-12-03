533b7a5ea4ebfef91b5f84689869c157a75d7683
Vladimir Sulovic <sulovic@gmail.com>
2024-12-04 00:01:59 +0100

    tested

--- utils/verifyGoogleToken.ts
+++ utils/verifyGoogleToken.ts
@@ -3,17 +3,12 @@ import { OAuth2Client, LoginTicket, TokenPayload } from "google-auth-library";
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const verifyGoogleToken = async (idToken: string): Promise<TokenPayload | undefined> => {
- 
-  console.log("received token", idToken);
- 
-  const ticket : LoginTicket = await client.verifyIdToken({
+  const ticket: LoginTicket = await client.verifyIdToken({
     idToken,
     audience: process.env.GOOGLE_CLIENT_ID,
   });
 
-  const payload : TokenPayload | undefined = ticket.getPayload();
-
-console.log("verified", payload);
+  const payload: TokenPayload | undefined = ticket.getPayload();
 
   return payload;
 };
