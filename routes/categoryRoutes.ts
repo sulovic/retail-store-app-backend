91044412bcc7fc216708d46b2dbf845661d3450f
Vladimir Sulovic <sulovic@gmail.com>
2025-05-20 08:40:52 +0200

    Fixed validation and productUrl field (temporary optional because of Prisma migration)

--- routes/categoryRoutes.ts
+++ routes/categoryRoutes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import categoryController from "../controllers/categoryController.js";
 import validateRequest from "../middleware/validateRequest.js";
-import { Category } from "../types/types.js";
+import { categorySchema } from "../types/types.js";
 
 const router = Router();
 
 router.get("/", categoryController.getAllCategoriesController);
 router.get("/count", categoryController.getAllCategoriesCountController);
 router.get("/:categoryId", categoryController.getCategoryController);
-router.post("/", validateRequest(Category), categoryController.createCategoryController);
-router.put("/:categoryId", validateRequest(Category), categoryController.updateCategoryController);
+router.post("/", validateRequest(categorySchema), categoryController.createCategoryController);
+router.put("/:categoryId", validateRequest(categorySchema), categoryController.updateCategoryController);
 router.delete("/:categoryId", categoryController.deleteCategoryController);
 
 export default router;
